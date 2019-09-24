df7937798b7cda7e1cd62c59c8863adb2eca8748
mobile <mobile@itcube.win>
2019-09-24 18:34:57 +0300

    hjkh

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a;
+        short b;
+        int v = 123_124_34;
+        byte f = 0;
+        long c = 47L;
+        float f1 = 43.8F;
+        double g = 34.6;
+        char a
+
     }
 }
